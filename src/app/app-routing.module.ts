5fd318ed4cc67839d6092daca10fece107235b71
Farid-lgs <farid.lagssaibi@gmail.com>
2021-06-08 15:47:16 +0200

    Ajout de add-center

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {CenterComponent} from "./component/center/center.component";
+import {AddCenterComponent} from "./component/center/add-center/add-center.component";
+import {UpdateCenterComponent} from "./component/center/update-center/update-center.component";
 
 
 const routes: Routes = [
-  { path: 'center/list', component:CenterComponent}
+  { path: 'center/list', component:CenterComponent},
+  { path: 'center/add', component:AddCenterComponent},
+  { path: 'center/update/:id', component:UpdateCenterComponent}
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { CenterComponent } from './component/center/center.component';
+import {AppComponent} from './app.component';
+import {CenterComponent} from './component/center/center.component';
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AddCenterComponent} from './component/center/add-center/add-center.component';
+import {UpdateCenterComponent} from './component/center/update-center/update-center.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CenterComponent
+    CenterComponent,
+    AddCenterComponent,
+    UpdateCenterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
