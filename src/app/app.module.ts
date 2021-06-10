940b21aa35e7fe30e3dfa3ec93a10feadf5c2a52
Farid-lgs <farid.lagssaibi@gmail.com>
2021-06-10 10:51:08 +0200

    Ajout des composents et services Trip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import {CenterComponent} from './component/center/center.component';
 import {AddCenterComponent} from './component/center/add-center/add-center.component';
 import {UpdateCenterComponent} from './component/center/update-center/update-center.component';
 import { FileUploadComponent } from './component/file-upload/file-upload.component';
+import { TripComponent } from './component/trip/trip.component';
+import { AddTripComponent } from './component/trip/add-trip/add-trip.component';
+import { UpdateTripComponent } from './component/trip/update-trip/update-trip.component';
+import { DetailTripComponent } from './component/trip/detail-trip/detail-trip.component';
 
 
 
@@ -25,7 +29,11 @@ import { FileUploadComponent } from './component/file-upload/file-upload.compone
     CenterComponent,
     AddCenterComponent,
     UpdateCenterComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    TripComponent,
+    AddTripComponent,
+    UpdateTripComponent,
+    DetailTripComponent
   ],
   imports: [
     BrowserModule,
