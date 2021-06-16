c685e5698534ea8729e05a77efada33ce575dc61
Farid-lgs <farid.lagssaibi@gmail.com>
2021-06-16 14:04:44 +0200

    correction add Trip et List Trip

--- src/app/component/trip/update-trip/update-trip.component.ts
+++ src/app/component/trip/update-trip/update-trip.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute} from "@angular/router";
 import {TripService} from "../../../service/trip.service";
 import {ClientService} from "../../../service/client.service";
 import {CenterService} from "../../../service/center.service";
+import {Center} from "../../../model/center/center";
+import {Client} from "../../../model/client/client";
 
 @Component({
   selector: 'app-update-trip',
@@ -14,30 +16,38 @@ export class UpdateTripComponent implements OnInit {
 
   updateTripForm: FormGroup;
   trip: any;
-  id: any;
+  centers: Center[];
+  center: Center;
+  clients: Client[];
+  client: Client;
+  tripId: any;
 
-  constructor(private formBuilder: FormBuilder, public tripService: TripService,public clientService: ClientService, public centerService: CenterService, private route: ActivatedRoute) {
+  constructor(private formBuilder: FormBuilder, public tripService: TripService, public clientService: ClientService, public centerService: CenterService, private route: ActivatedRoute) {
   }
+
   ngOnInit(): void {
     this.recupData();
-
+    this.recupDataCLients();
+    this.recupDataCenters();
     setTimeout(() => {
       this.form();
     }, 1000);
+
   }
+
   form() {
     this.updateTripForm = this.formBuilder.group({
-      tripid: [this.trip.tripid, Validators.required],
+      tripId: [this.tripId, Validators.required],
       tripStartDate: [this.trip.tripStartDate, Validators.required],
       tripEndDate: [this.trip.tripEndDate, Validators.required],
-      center: [this.trip.center.centerid, Validators.required],
+      center: [this.trip.centerId, Validators.required],
       client: [this.trip.client.clientId, Validators.required],
     });
   }
 
   recupData() {
-    this.id = this.route.snapshot.paramMap.get('id');
-    this.trip = this.centerService.findById(this.id).subscribe(
+    this.tripId = this.route.snapshot.paramMap.get('id');
+    this.trip = this.tripService.findById(this.tripId).subscribe(
       (trip) => {
         this.trip = trip;
       },
@@ -46,8 +56,46 @@ export class UpdateTripComponent implements OnInit {
       }
     );
   }
+
+  recupDataCLients() {
+    this.clientService.findAll().subscribe(
+      (clients) => {
+        this.clients = clients;
+      },
+      (error) => {
+        console.log('error = ' + error.message);
+      }
+    );
+  }
+
+  recupDataCenters() {
+    this.centerService.findAll().subscribe(
+      (centers) => {
+        this.centers = centers;
+      },
+      (error) => {
+        console.log('error = ' + error.message);
+      }
+    );
+  }
+
   onSubmit() {
     const data = this.updateTripForm.value;
-    this.centerService.update(this.id,data);
+    setTimeout(() => {
+      this.client = {
+        clientId: data.clientId
+      }
+      this.center = {
+        centerId: data.centerId
+      }
+
+      this.trip = {
+        tripStartDate: data.tripStartDate,
+        tripEndDate: data.tripEndDate,
+        center: this.center,
+        client: this.client,
+      }
+      this.tripService.update(this.tripId, this.trip);
+    }, 1000);
   }
 }
